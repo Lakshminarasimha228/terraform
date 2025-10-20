5b513d7e68832d545c0045b36a3b66a14a282a33
lakshmi <lakshminarasimha228@gmail.com>
2025-10-21 00:46:07 +0530

    terraform

---
+++ roboshop-ec2-test/variables.tf
@@ -0,0 +1,15 @@
+variable "security_group_ids" {
+    default = ["sg-032739b3c91414e33"]
+}
+
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
